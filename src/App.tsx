321351a3f7ee5a02bd14520d19c5da2d282312d5
Vivek <vivek29876@gmail.com>
2024-09-17 23:19:29 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 // src/App.js
-import React from 'react';
-import Dashboard from './components/Dashboard';
+import CheckboxRowSelectionDemo from '../src/components/CheckboxRowSelectionDemo';
 
 function App() {
     return (
         <div className="bg-gray-100 min-h-screen">
             <div className="container mx-auto p-4">
                 <h1 className="text-3xl font-bold mb-4">User Dashboard</h1>
-                <Dashboard />
+                <CheckboxRowSelectionDemo />
             </div>
         </div>
     );
