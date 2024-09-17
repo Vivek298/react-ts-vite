0ec29edbb5b54c7106bb7830e34c2f23f1410b9e
Vivek <vivek29876@gmail.com>
2024-09-17 23:00:24 +0530

    error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import CheckboxRowSelectionDemo from "./components/CheckboxRowSelectionDemo"
+// src/App.js
+import React from 'react';
+import Dashboard from './components/Dashboard';
 
 function App() {
-
-  return (
-    <>
-      <CheckboxRowSelectionDemo />
-    </>
-  )
+    return (
+        <div className="bg-gray-100 min-h-screen">
+            <div className="container mx-auto p-4">
+                <h1 className="text-3xl font-bold mb-4">User Dashboard</h1>
+                <Dashboard />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
